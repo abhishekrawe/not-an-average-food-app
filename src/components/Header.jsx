3391d8260d42ad768091e4d3b9d86f064acbd47e
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-12-26 23:25:14 +0530

    Header Initail - UI

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch , faGift, faShoppingBag, faUser, faGear } from "@fortawesome/free-solid-svg-icons";
+
+function Header() {
+  return (
+    <>
+      <div className="px-4 md:px-36 py-4 bg-white shadow-md">
+        <div className="container mx-auto flex flex-col md:flex-row md:items-center">
+
+          <div className="flex justify-between items-center text-gray-600">
+              <img src="https://logolook.net/wp-content/uploads/2023/04/Swiggy-Emblem.png" alt="logo" className="h-12" />
+            <p><span className="text-md font-semibold mt-2 md:mt-0 md:ml-4 mr-2 underline hover:text-orange-500">Patna</span> RMS Colony Road No. 1 Kankarbagh Patna -20</p>
+          </div>
+
+          <div className="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0">
+            <a href="#" className="p-2 lg:px-4 md:mx-2 text-orange-500"><FontAwesomeIcon icon={faSearch} className="mr-4" />Search</a>
+            <a href="#" className="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:text-orange-500 transition-colors duration-300"><FontAwesomeIcon icon={faGift} className="mr-4" />Offers</a>
+            <a href="#" className="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:text-orange-500 transition-colors duration-300"><FontAwesomeIcon icon={faGear} className="mr-4" />Help</a>
+            <a href="#" className="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:text-orange-500 transition-colors duration-300"><FontAwesomeIcon icon={faUser} className="mr-4" />Sign In</a>
+            <a href="#" className="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:text-orange-500 transition-colors duration-300"><FontAwesomeIcon icon={faShoppingBag} className="mr-4" />Cart</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
