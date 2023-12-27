4b655c094f8c350abfe1837452c92436826567d9
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-12-28 01:47:50 +0530

    mapped the data and used add info to the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,76 +1,38 @@
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock, faStar } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
+import { restaurantsList } from "../constant";
 
 function Card() {
     return (
         <>
-            <div className="px-4 md:px-36 py-6">
-                <div className="justify-between flex my-6">
-                    <div className="text-2xl font-bold text-left">
+            <div className="px-4 md:px-36">
+                <div className="justify-between flex my-4">
+                    <div className="text-3xl font-bold text-left">
                         <h1>Restaurants with online food delivery in Patna</h1>
                     </div>
+                    <div className="flex gap-4">
+                        <input type="Search for resturents and food" className="w-full p-4 ps-10 text-sm rounded-lg border border-gray-700 " />
+                        <button className="bg-orange-500 px-4 rounded-lg text-white">Search </button>
+                    </div>
                 </div>
-                <div className="flex gap-4">
-                    <div>
-                        <div className="block max-w-md rounded-sm shadow">
-                            <a href="#">
-                                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/tnjwc1hltzaudqyilpzq" alt="Product" class="h-60 w-full rounded-xl" />
-                            </a>
-                        </div>
-                        <div class="px-4 py-4">
-                            <p class="text-xl font-bold text-gray-700 truncate block capitalize text-left">La Pino'z Pizza</p>
-                            <p class="text-lg font-bold text-gray-700 truncate block capitalize text-left"> <FontAwesomeIcon icon={faStar} className="mr-2 text-green-600" />4.1 <span> <FontAwesomeIcon icon={faClock} className="mr-2" />25-30 mins</span></p>
-                            <p class="text-gray-400 text-left">Pizzas, Pastas, italians, Desert , Kankrabgah</p>
-                        </div>
+
+                <div class="container my-6 mx-auto px-4 md:px-12">
+                    <div class="flex flex-wrap -mx-1 lg:-mx-16">
+                        {restaurantsList.map((restaurant) => (
+                            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
+                                <a href="#">
+                                    <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restaurant.cloudinaryImageId}`} alt="Product" class="h-52 w-full rounded-xl" />
+                                </a>
+                                <p class="text-xl font-bold text-gray-700 truncate block capitalize text-left">{restaurant.name}</p>
+                                <p class="text-lg font-bold text-gray-700 truncate block capitalize text-left"> <FontAwesomeIcon icon={faStar} className="mr-2 text-green-600" />{restaurant.avgRating} <span> <FontAwesomeIcon icon={faClock} className="mr-2" />{restaurant.sla.slaString}</span></p>
+                                <p class="text-gray-400 text-left">{restaurant.cuisines.join(', ')}</p>
+                                <p class="text-gray-400 text-left">{restaurant.areaName}</p>
+                            </div>
+                        ))}
 
                     </div>
-
-                    <div>
-                        <div className="block max-w-md rounded-sm shadow">
-                            <a href="#">
-                                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/tnjwc1hltzaudqyilpzq" alt="Product" class="h-60 w-full rounded-xl" />
-                            </a>
-                        </div>
-                        <div class="px-4 py-4">
-                            <p class="text-xl font-bold text-gray-700 truncate block capitalize text-left">La Pino'z Pizza</p>
-                            <p class="text-lg font-bold text-gray-700 truncate block capitalize text-left"> <FontAwesomeIcon icon={faStar} className="mr-2 text-green-600" />4.1 <span> <FontAwesomeIcon icon={faClock} className="mr-2" />25-30 mins</span></p>
-                            <p class="text-gray-400 text-left">Pizzas, Pastas, italians, Desert , Kankrabgah</p>
-                        </div>
-
-                    </div>
-
-                    <div>
-                        <div className="block max-w-md rounded-sm shadow">
-                            <a href="#">
-                                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/tnjwc1hltzaudqyilpzq" alt="Product" class="h-60 w-full rounded-xl" />
-                            </a>
-                        </div>
-                        <div class="px-4 py-4">
-                            <p class="text-xl font-bold text-gray-700 truncate block capitalize text-left">La Pino'z Pizza</p>
-                            <p class="text-lg font-bold text-gray-700 truncate block capitalize text-left"> <FontAwesomeIcon icon={faStar} className="mr-2 text-green-600" />4.1 <span> <FontAwesomeIcon icon={faClock} className="mr-2" />25-30 mins</span></p>
-                            <p class="text-gray-400 text-left">Pizzas, Pastas, italians, Desert , Kankrabgah</p>
-                        </div>
-
-                    </div>
-
-                    <div>
-                        <div className="block max-w-md rounded-sm shadow">
-                            <a href="#">
-                                <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/tnjwc1hltzaudqyilpzq" alt="Product" class="h-60 w-full rounded-xl" />
-                            </a>
-                        </div>
-                        <div class="px-4 py-4">
-                            <p class="text-xl font-bold text-gray-700 truncate block capitalize text-left">La Pino'z Pizza</p>
-                            <p class="text-lg font-bold text-gray-700 truncate block capitalize text-left"> <FontAwesomeIcon icon={faStar} className="mr-2 text-green-600" />4.1 <span> <FontAwesomeIcon icon={faClock} className="mr-2" />25-30 mins</span></p>
-                            <p class="text-gray-400 text-left">Pizzas, Pastas, italians, Desert , Kankrabgah</p>
-                        </div>
-
-                    </div>
-
                 </div>
-                
-
             </div>
         </>
 
