1c3877fcfdd631d379d28d3814596b2ad83e80b9
abhishekrawe <rajputabhisheksingh53@gmail.com>
2024-01-15 15:19:58 +0530

    created routes for pages index and App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
 import "./App.css";
 import Card from "./components/Card";
 import Category from "./components/Category";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Offers from "./components/Offers";
-function App() {
+import Cart from "./pages/Cart";
+import Help from "./pages/Help";
+import Offer from "./pages/Offers";
+import Signin from "./pages/Signin";
+import Error from "./pages/Error";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RestaurantMenu from "./components/RestaurantMenu";
+
+const App = () => {
   return (
     <div className="App">
-      <Header/>
-      <Offers/>
-      <Category/>
-      <Card/>
-      <Footer/>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/offer" element={<Offer />} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/restaurant/:id" element={<RestaurantMenu/>} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      <Footer />
     </div>
   );
 }
+
+const Home = () => (
+  <>
+    <Offers />
+    <Category />
+    <Card />
+  </>
+);
+
 
 export default App;
