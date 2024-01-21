8ee315c90c4f14ba8e02aaefcc421a20295b5af3
abhishekrawe <rajputabhisheksingh53@gmail.com>
2024-01-21 19:24:14 +0530

    Build Error Resolved

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -9,15 +9,15 @@ const Cart = () => {
     <>
       <h1 className="pt-24 text-3xl font-bold text-left px-4">  Total Items - {cartItems.length} </h1>
       <div className="flex">
-          <div className="justify-center flex">
-            <div className="flex flex-wrap">
-              {cartItems.map((card, index) => (
-                <FoodItem key={index}
-                  title={card.card.card.title}
-                  price={card.card.card.itemCards[0].card.info.price / 100}
-                  image={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${card.card.card.itemCards[0].card.info.imageId}`} />
-              ))}
-            </div>
+        <div className="justify-center flex">
+          <div className="flex flex-wrap">
+            {cartItems.map((card, index) => (
+              <FoodItem key={index}
+                title={card.card.card.title}
+                price={card.card.card.itemCards[0].card.info.price / 100}
+                image={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${card.card.card.itemCards[0].card.info.imageId}`} />
+            ))}
+          </div>
         </div>
       </div>
 
